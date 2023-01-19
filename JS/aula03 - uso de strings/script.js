aa6493ecd1641568fa9d1ee054d96cda32977a08
Jorge H <jorgehenrrique@live.com>
2023-01-19 20:49:14 -0300

    aula 03 strings

---
+++ JS/aula03 - uso de strings/script.js
@@ -0,0 +1,14 @@
+{ // Exercícios
+    let string1 = "Manipulando strings com JS"; // a
+    
+    alert(string1.toUpperCase()); // b
+    
+    console.log(string1.slice(0,5)); // c
+
+    alert(string1.length); // d
+
+    let string2 = 'Vamos concatenar as duas strings'; // e
+
+    console.log(`${string1} ${string2}`); // f, opcao 01
+    console.log(string1 + ' ' + string2); // f, opcao 02
+}
