4aafe9e3d0ccde5ec607bc98f686d0ba166ebb10
Jorge H <jorgehenrrique@live.com>
2023-01-25 21:44:26 -0300

    adc aula 06

--- JS/aula06-Estruturas de repetição (While e do while)/index.js
+++ JS/aula06-Estruturas de repetição (While e do while)/index.js
@@ -3,14 +3,13 @@
     // console.log(typeof anosNaEscola);
     if (isNaN(anosNaEscola)) {
         alert('Voce deve digitar apenas números para receber o dado correto.');
-    }else {
-        anosNaEscola = parseInt(anosNaEscola);
     }
     // console.log(typeof anosNaEscola);
 
     let reciclagens = anosNaEscola / 2;
+    reciclagens = parseInt(reciclagens);
 
-    if (reciclagens < 2) {
+    if (anosNaEscola < 2) {
         alert('Voce ainda nao tem o tempo minimo de 2 anos para ter uma reciclagem.');
     } else if(reciclagens) {
         alert(`Esse piloto particiou de: ${reciclagens} reciclagens.`);
