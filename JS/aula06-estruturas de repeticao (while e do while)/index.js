75559967787273bc29a232e97994362075e924e9
Jorge H <jorgehenrrique@live.com>
2023-01-26 21:33:46 -0300

    adc aula 07

--- JS/aula06-estruturas de repeticao (while e do while)/index.js
+++ JS/aula06-estruturas de repeticao (while e do while)/index.js
@@ -1,19 +1,3 @@
-// {
-//     let anosNaEscola = prompt('Quantos anos voce esta vinculado na escola?');
-
-//     if (isNaN(anosNaEscola)) {
-//         alert('Voce deve digitar apenas números para receber o dado correto.');
-//     }
-
-//     let reciclagens = anosNaEscola / 2;
-//     reciclagens = parseInt(reciclagens);
-
-//     if (anosNaEscola < 2) {
-//         alert('Voce ainda nao tem o tempo minimo de 2 anos para ter uma reciclagem.');
-//     } else if(reciclagens) {
-//         alert(`Esse piloto particiou de: ${reciclagens} reciclagens.`);
-//     }
-// }
 {
     let finaliza = true;
     while (finaliza){
---
+++ JS/aula07-estruturas de repeticao (for)/index.js
@@ -0,0 +1,29 @@
+// corrido mach1
+// posicao dos ganhadores
+// devem digitar os nomes nos pilotos separados por , Ex: piloto1,piloto2
+
+// receber essa lista
+// devolver a posicao e o nome do piloto
+// os 3 primeiros exibir pódio
+
+{
+    let piloto = prompt('Informe os nomes dos pilotos, separados por virgula: ');
+
+    if (!isNaN(piloto)){
+        alert('Voce digitou algo errado, escreva os nomes.');
+        window.location.reload()
+    }else {
+        let nomePilotos = piloto.split(',');
+
+        let exibirListaPilotos = '';
+
+        for (posicao = 0; posicao < nomePilotos.length; posicao++){
+            if (posicao < 3){
+                exibirListaPilotos += 'Pódio - ' + (posicao+1) + ' - ' + nomePilotos[posicao]+ '\n';
+            }else{
+                exibirListaPilotos += posicao+1 + ' - ' + nomePilotos[posicao]+ '\n';
+            }
+        }
+        alert('Lista dos pilotos:\n'+ exibirListaPilotos);
+    }
+}
