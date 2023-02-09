96dcd45e7b5db8c5588367587bdb0d94a29ee816
Jorge H <jorgehenrrique@live.com>
2023-02-08 22:17:13 -0300

    adc exercicio aula 13

---
+++ JS/aula13-funcoes03/script.js
@@ -0,0 +1,56 @@
+// Calculadora de desconto mensal de IR
+// Usuario digita seu salario
+// Devolver o desconto de IR correspondente com base na tabela
+/* * Requisitos:
+● Você deverá utilizar Arrow Function e Callback na sua solução.
+● Fique atento nas regras do IR para atender os cenários propostos.
+● A organização do seu código e a nomenclatura das variáveis/funções será avaliada.
+● Leiam e compreendam a regra de negócio para desconto de imposto de renda retido na fonte. */
+
+let exibeDescontoIR = (retorno) => console.log(`${retorno}`);
+
+
+function recebeValida(exibeRetorno) {
+    // let salarioMensal = prompt('Informa seu salario mensal: ');
+    let salarioMensal = prompt('Informe seu salário mensal: ');
+    let taxaIr = 0;
+    let taxaDesconto = 0;
+    let salarioDescontado = 0;
+    let retorno = 0;
+    // if (salarioMensal <= 1903) {
+    //     retorno = (`Isento, salario: R$${salarioMensal}`);
+    // } else 
+    if (salarioMensal > 1903 && salarioMensal <= 2826) {
+        taxaIr = 0.075;
+        taxaDesconto = salarioMensal * taxaIr;
+        taxaDesconto -= 142;
+        salarioDescontado = salarioMensal - taxaDesconto;
+        // retorno = (`Salario descontato: R$${salarioDescontado.toFixed(2)}, desconto: R$${taxaDesconto}`);
+    } else if (salarioMensal > 2826 && salarioMensal <= 3751) {
+        taxaIr = 0.15;
+        taxaDesconto = salarioMensal * taxaIr;
+        taxaDesconto -= 354;
+        salarioDescontado = salarioMensal - taxaDesconto;
+        // retorno = (`Salario descontato: R$${salarioDescontado.toFixed(2)}, desconto: R$${taxaDesconto}`);
+    } else if (salarioMensal > 3751 && salarioMensal <= 4664) {
+        taxaIr = 0.225;
+        taxaDesconto = salarioMensal * taxaIr;
+        taxaDesconto -= 636;
+        salarioDescontado = salarioMensal - taxaDesconto;
+        // retorno = (`Salario descontato: R$${salarioDescontado.toFixed(2)}, desconto: R$${taxaDesconto}`);
+    } else {
+        taxaIr = 0.275;
+        taxaDesconto = salarioMensal * taxaIr;
+        taxaDesconto -= 869;
+        salarioDescontado = salarioMensal - taxaDesconto;
+        // retorno = (`Salario descontato: R$${salarioDescontado.toFixed(2)}, desconto: R$${taxaDesconto.toFixed(2)}`);
+    }
+    if (salarioMensal <= 1903) {
+        retorno = (`Isento\nSalario: R$${salarioMensal}`);
+    } else {
+        retorno = (`Salario descontato: R$${salarioDescontado.toFixed(2)}\nDesconto: R$${taxaDesconto.toFixed(2)}`);
+    }
+    exibeRetorno(retorno);
+}
+
+recebeValida(exibeDescontoIR);
