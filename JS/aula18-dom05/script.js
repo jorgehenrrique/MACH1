3d25db765226678469b7adec8c74eda90c897e7e
Jorge H <jorgehenrrique@live.com>
2023-02-16 22:54:21 -0300

    inicio de desenvolvimento do JS

---
+++ JS/aula18-dom05/script.js
@@ -0,0 +1,9 @@
+// || Inputs
+const inputNome = document.querySelector('#nome');
+const inputEmail = document.querySelector('#email');
+const inputTel = document.querySelector('#tel');
+
+// || Botoes
+const btnSalvar = document.querySelector('.btn');
+const btnConcluir = document.querySelector('.btn-concluir');
+const btnVoltar = document.querySelector('.btn-voltar');
