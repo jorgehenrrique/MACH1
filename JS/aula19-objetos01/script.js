57ab8272dc1407c3c466517f02c63def4908fe6f
Jorge H <jorgehenrrique@live.com>
2023-02-28 21:48:52 -0300

    adc Aula 19

---
+++ JS/aula19-objetos01/script.js
@@ -0,0 +1,45 @@
+const listaUsuarios = [
+    {
+        id: 1,
+        nome: 'Thiago Pereira',
+        sexo: 'Masculino',
+        idade: 20,
+        email: 'fulanodetal@termail.com'
+    },
+    {
+        id: 2,
+        nome: 'Thais Lins',
+        sexo: 'Feminino',
+        idade: 19,
+        email: 'thaisdetal@gtemail.com'
+    },
+    {
+        id: 3,
+        nome: 'Mariana Ferreira',
+        sexo: 'Feminino',
+        idade: 25,
+        email: 'marianaferreirs@tailme.com'
+    },
+    {
+        id: 4,
+        nome: 'Pedro Ivo Silva',
+        sexo: 'Masculino',
+        idade: 28,
+        email: 'pedroivo@gtepmail.com'
+    }
+];
+
+resultado = document.querySelector('#resultado');
+
+listaUsuarios.forEach(listarUsuario);
+
+function listarUsuario(usuario) {
+
+    resultado.innerHTML += `<tr>
+        <td>${usuario.id}</td>
+        <td>${usuario.nome}</td>
+        <td>${usuario.sexo}</td>
+        <td>${usuario.idade}</td>
+        <td>${usuario.email}</td>
+    </tr>`;
+}
