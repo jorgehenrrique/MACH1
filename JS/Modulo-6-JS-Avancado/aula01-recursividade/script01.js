a3ccf24b4a12cc885047bddf95c307fe3ebb55fd
Jorge H <jorgehenrrique@live.com>
2023-03-27 22:03:18 -0300

    add atividade pratica recursividade01

---
+++ JS/Modulo-6-JS-Avancado/aula01-recursividade/script01.js
@@ -0,0 +1,20 @@
+// Atividade pratica Recursividade - 01
+
+const clientes = ['Jorge', 'Maria', 'Manoel', 'João', 'Roberto', 'Vera'];
+
+let minutos = 15;
+function calcularTempoSaida(arr, cliente) {
+    if (arr[arr.length - 1] == cliente) {
+        console.log(minutos);
+        return minutos;
+    } else {
+        arr.pop()
+        minutos += 15;
+        return (arr.length <= 0) ? console.log('null') : calcularTempoSaida(arr, cliente);
+    }
+}
+
+calcularTempoSaida(clientes, 'Vera') // Resultado (15)
+calcularTempoSaida(clientes, 'Manoel') // Resultado (60)
+calcularTempoSaida(clientes, 'Jorge') // Resultado (90)
+calcularTempoSaida(clientes, 'Jonas') // Resultado (null)
---
+++ JS/Modulo-6-JS-Avancado/aula01-recursividade/script02.js
@@ -0,0 +1,1 @@
+// Atividade pratica Recursividade - 02
