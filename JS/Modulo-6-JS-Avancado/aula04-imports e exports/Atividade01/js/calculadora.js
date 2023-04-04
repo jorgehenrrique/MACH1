04aa25cfaf42efd0bb4dbf576942850a5f57ae9e
Jorge H <jorgehenrrique@live.com>
2023-04-03 21:17:08 -0300

    adc atividade pratica 01

---
+++ JS/Modulo-6-JS-Avancado/aula04-imports e exports/Atividade01/js/calculadora.js
@@ -0,0 +1,6 @@
+import * as calculadora from '../modules/operacoes.mjs';
+
+console.log(calculadora.somar(4, 2));
+console.log(calculadora.subtrair(4, 2));
+console.log(calculadora.dividir(4, 2));
+console.log(calculadora.multiplicar(4, 2));
