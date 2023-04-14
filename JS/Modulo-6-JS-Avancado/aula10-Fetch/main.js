68f02ea2ebc7d7470fee0bc8ac922b298a044bf8
Jorge H <jorgehenrrique@live.com>
2023-04-13 21:37:17 -0300

    terminada func de excluir

--- JS/Modulo-6-JS-Avancado/aula10-Fetch/main.js
+++ JS/Modulo-6-JS-Avancado/aula10-Fetch/main.js
@@ -1,6 +1,7 @@
 const optBuscaNome = document.querySelector("#opt-busca-nome")
 const optBuscaTodos = document.querySelector("#opt-busca-todos")
 const filtersNome = document.querySelector("#pn-filter-nome");
+// const btnExcluir = document.querySelectorAll('.btnExcluir');
 
 import { inputMatricula, printNome, printMatricula, btnBuscarMatricula, inputBuscarNome, btnBuscar, printDados, divDados, loaderConsulta } from "./modules/elementos.js";
 
@@ -21,23 +22,23 @@ const buscarMatricula = () => {
     if (matriculaBusca) {
         buscaPorMatricula(matriculaBusca);
         divDados.style.display = 'none';
-        loaderConsulta.style.display = 'block'
+        loaderConsulta.style.display = 'block';
     } else {
-        console.warn('Digite a matrigula')
+        console.warn('Digite a matrigula');
     }
 }
 
 const buscarNome = () => {
     let nomeBusca = inputBuscarNome.value.trim();
     if (nomeBusca) {
-        console.log('Buscando nome')
+        console.log('Buscando nome');
         buscaPorNome(nomeBusca);
-        loaderConsulta.style.display = 'block'
+        loaderConsulta.style.display = 'block';
     } else {
-        console.log('Buscando todos')
+        console.log('Buscando todos');
         buscaPorTodos();
-        divDados.style.display = 'none';
-        loaderConsulta.style.display = 'block'
+        divDados.style.display = 'none';;
+        loaderConsulta.style.display = 'block';
     }
 }
 btnBuscarMatricula.onclick = buscarMatricula;
@@ -45,19 +46,20 @@ btnBuscar.onclick = buscarNome;
 
 function buscaPorMatricula(matricula) {
     const urlBuscaM = `http://192.168.50.99:3000/aluno/${matricula}`;
-    const options = {
+    const options = { // Nao é preciso para o get, pode excluir
         method: "GET",
         mode: "cors",
         cache: "no-cache"
     };
+    // o options abaixo no fetch, pode ser excluido
     fetch(urlBuscaM, options).then((response) => {
         if (response.ok && response.status === 200) {
-            loaderConsulta.style.display = 'none'
+            loaderConsulta.style.display = 'none';
             return response.json();
         } else {
             // console.error('Matricula não encontrada');
-            loaderConsulta.style.display = 'none'
-            alert('Matricula não encontrada')
+            loaderConsulta.style.display = 'none';
+            alert('Matricula não encontrada');
         }
     }).then((data) => {
         // console.log(data)
@@ -75,25 +77,25 @@ function buscaPorMatricula(matricula) {
 
 function buscaPorNome(nome) {
     const urlBuscaN = `http://192.168.50.99:3000/aluno/search?nome=${nome}`;
-    const options = {
-        method: "GET",
-        mode: "cors",
-        cache: "no-cache"
-    };
-    fetch(urlBuscaN, options).then((response) => {
+    // const options = {
+    //     method: "GET",
+    //     mode: "cors",
+    //     cache: "no-cache"
+    // };
+    fetch(urlBuscaN).then((response) => {
         if (response.ok && response.status === 200) {
             return response.json();
         } else {
             // console.error('Nome não encontrado');
-            loaderConsulta.style.display = 'none'
-            alert('Nome não encontrado')
+            loaderConsulta.style.display = 'none';
+            alert('Nome não encontrado');
         }
     }).then((data) => {
         if (data.length <= 0) {
-            loaderConsulta.style.display = 'none'
+            loaderConsulta.style.display = 'none';
             printDados.innerHTML = '';
             printDados.innerHTML = `<tr><td colspan="2">Nenhuma informação encontrada.</td></tr>`;
-            alert('Nome não encontrado')
+            alert('Nome não encontrado');
             throw new Error('Não foi possível consultar essa informacao');
         }
         divDados.style.display = 'block';
@@ -103,40 +105,62 @@ function buscaPorNome(nome) {
 }
 
 function imprimeAlunos(data) {
-    loaderConsulta.style.display = 'none'
+    loaderConsulta.style.display = 'none';
     for (let aluno in data) {
-        printDados.innerHTML += `<tr><td>Nome: ${data[aluno].nome} - Matricula: ${data[aluno].matricula} - <button onclick="excluir(${data[aluno].matricula})">Excluir aluno</button></td></tr>`;
+        printDados.innerHTML += `<tr><td>Nome: ${data[aluno].nome} - Matricula: ${data[aluno].matricula} - <button id="${data[aluno].matricula}" class="btnExcluir">Excluir aluno</button></td></tr>`;
     }
+    document.querySelectorAll('.btnExcluir').forEach(btn => {
+        btn.addEventListener('click', () => excluir(btn.id))
+    })
+}
+function excluir(matricula) {
+    loaderConsulta.style.display = 'block';
+    const urlDelete = `http://192.168.50.99:3000/aluno/deletar?matricula=${matricula}`;
+    const options = {
+        method: "POST"
+    };
+    fetch(urlDelete, options).then((response) => {
+        if (response.ok && response.status === 200) {
+            console.log('ok??!!')
+            return response.json();
+        }
+        if (response.status === 404) {
+            throw new Error("Matricula não encontrada");
+        }
+    }).then((response) => {
+        if (response.result) {
+            alert("Aluno escluido com sucesso!");
+            buscaPorTodos();
+        }
+    }).catch((error) => {
+        alert(error.message);
+    })
 }
 
 function buscaPorTodos() {
     const urlBuscaT = `http://192.168.50.99:3000/aluno/todos`;
-    const options = {
-        method: "GET",
-        mode: "cors",
-        cache: "no-cache"
-    };
-    fetch(urlBuscaT, options).then((response) => {
+    // const options = {
+    //     method: "GET",
+    //     mode: "cors",
+    //     cache: "no-cache"
+    // };
+    fetch(urlBuscaT).then((response) => {
         if (response.ok && response.status === 200) {
             return response.json();
         } else {
             // console.error('Dados não encontrados');
-            loaderConsulta.style.display = 'none'
+            loaderConsulta.style.display = 'none';
             alert('Dados não encontrado')
         }
     }).then((data) => {
         if (data.length <= 0) {
-            loaderConsulta.style.display = 'none'
+            loaderConsulta.style.display = 'none';
             printDados.innerHTML = '';
             printDados.innerHTML = `<tr><td colspan="2">Nenhuma informação encontrada.</td></tr>`;
             throw new Error('Não foi possível consultar essa informacao');
         }
-        divDados.style.display = 'block'
+        divDados.style.display = 'block';
         printDados.innerHTML = '';
         imprimeAlunos(data);
     })
-}
-
-function excluir(matricula) {
-    console.log(matricula)
 }
