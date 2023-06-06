8fc1c559bda58f3358a5df078efc4b5f149e4e37
Jorge H <jorgehenrrique@live.com>
2023-06-06 15:01:52 -0300

    reajuste comentario

--- REACT/Modulo-7-React/aula08-consumindo-api/src/components/MyComponent.jsx
+++ REACT/Modulo-7-React/aula08-consumindo-api/src/components/MyComponent.jsx
@@ -12,12 +12,12 @@ export default function MyComponent() {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
         const jsonData = await response.json();
 
-        setTimeout(() => { // adicionando mais atrasa de 1s
+        setTimeout(() => { // adicionando mais atraso de 300ms
           setData(jsonData); // passando lista para a variavel de estado
         }, 300);
       } catch (error) {
         console.log('Erro ao buscar os dados:', error);
-        setErro('Erro ao buscar os dados'); // Exibe esse erro
+        setErro('Erro ao buscar os dados'); // Passando erro para var de estado erro
       }
     };
 
@@ -28,13 +28,15 @@ export default function MyComponent() {
       setData(null); // Limpa a lista
       setErro(null); // Limpa o erro
     };
-  }, []); // Será executado apenas uma vez, ao montar o componente
+  }, []); // Será executado ao montar o componente
 
   return (
     <div>
       {data ? (
         <ul>{data.map(item =>
-          (<li key={item.id}>{item.title} <NavLink state={item} to={`/frutas/${item.id}`}>FRUTA</NavLink></li>))}</ul>)
+        (<li key={item.id}>{item.title}
+          <NavLink state={item} to={`/frutas/${item.id}`}>FRUTA</NavLink>
+        </li>))}</ul>)
         : (<span className="loader"></span>)}
       {erro &&
         <p>{erro}</p>}
