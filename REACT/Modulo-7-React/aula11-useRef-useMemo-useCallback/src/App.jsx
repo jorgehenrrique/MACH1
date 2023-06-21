f83323a2e9c56fda5ed8031c014a7ad45e06e142
Jorge H <jorgehenrrique@live.com>
2023-06-21 14:04:16 -0300

    atualizando atividade pratica 11

---
+++ REACT/Modulo-7-React/aula11-useRef-useMemo-useCallback/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import ClickCounter from './ClickCounter';
+import InputCounter from './InputCounter';
+import ProductList from './ProductList';
+
+function App() {
+
+  return (
+    <>
+      <InputCounter />
+      <ClickCounter />
+      <ProductList />
+    </>
+  )
+}
+
+export default App;
---
+++ REACT/Modulo-7-React/aula11-useRef-useMemo-useCallback/src/ProductList.jsx
@@ -0,0 +1,36 @@
+import { useState, useMemo } from 'react';
+
+export default function ProductList() {
+  const [productList, setProductList] = useState(['Product-1', 'Product-2', 'Product-3', 'Product-4']);
+
+  const addProduct = () => {
+    setProductList([...productList, 'Product-' + (productList.length + 1)]);
+  };
+  const rmvProduct = () => {
+    setProductList(productList.slice(0, -1));
+  };
+
+  const products = useMemo(() => {
+    return (
+      <ul>
+        {productList.map((product, idx) => {
+          return (
+            <li key={idx}>{product}</li>
+          );
+        })}
+      </ul>
+    );
+  }, [productList]);
+
+  return (
+    <>
+      <div className="container">
+        <h2>Aplicando useMemo</h2>
+        <button onClick={addProduct}>Adicionar Produto</button>
+        <button onClick={rmvProduct}>Remover Último Produto</button>
+
+        {products}
+      </div>
+    </>
+  );
+}
