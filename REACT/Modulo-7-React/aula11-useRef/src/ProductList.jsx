e41422aba6528d6cf657a58a102f4dd0a9d81e0c
Jorge H <jorgehenrrique@live.com>
2023-06-21 14:02:21 -0300

    atualizando atividade pratica 11

--- REACT/Modulo-7-React/aula11-useRef/src/ProductList.jsx
+++ REACT/Modulo-7-React/aula11-useRef/src/ProductList.jsx
@@ -1,13 +1,35 @@
+import { useState, useMemo } from 'react';
 
 export default function ProductList() {
+  const [productList, setProductList] = useState(['Product-1', 'Product-2', 'Product-3', 'Product-4']);
 
+  const addProduct = () => {
+    setProductList([...productList, 'Product-' + (productList.length + 1)]);
+  };
+  const rmvProduct = () => {
+    setProductList(productList.slice(0, -1));
+  };
+
+  const products = useMemo(() => {
+    return (
+      <ul>
+        {productList.map((product, idx) => {
+          return (
+            <li key={idx}>{product}</li>
+          );
+        })}
+      </ul>
+    );
+  }, [productList]);
 
   return (
     <>
       <div className="container">
         <h2>Aplicando useMemo</h2>
+        <button onClick={addProduct}>+</button>
+        <button onClick={rmvProduct}>-</button>
 
-
+        {products}
       </div>
     </>
   );
