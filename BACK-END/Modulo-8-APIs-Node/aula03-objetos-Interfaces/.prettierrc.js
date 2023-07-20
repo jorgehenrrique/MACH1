0b22f82a2477685682cfc1ba0eedbd3d6307d9bf
Jorge H <jorgehenrrique@live.com>
2023-07-19 21:47:45 -0300

    add atividade pratica 03

---
+++ BACK-END/Modulo-8-APIs-Node/aula03-objetos-Interfaces/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // add ponto e virgula
+  trailingComma: 'all', // virgula no final do obj
+  singleQuote: true, // aspas simples
+  printWidth: 80, // largura máxima da linha para quebrar
+  tabWidth: 2, // largura do tab
+};
---
+++ BACK-END/Modulo-8-APIs-Node/aula03-objetos-Interfaces/src/index.ts
@@ -0,0 +1,52 @@
+/*
+Vamos fazer um algoritmo para cadastro de clientes, nele você deverá receber primeiramente o CPF do cliente, e se válido deverá coletar os demais dados como:
+Nome,Idade,Estado Civil,Endereço, Cidade e Estado.
+
+
+Para executar essa tarefa você deverá utilizar interface no typescript;
+
+Considere válido CPF sem formatação (sem pontuação ou máscara) contendo 11 dígitos;
+
+Caso o CPF seja inválido de um retorno adequado ao usuário que está preenchendo;
+
+Os dados devem ser recebidos através do terminal.
+*/
+
+const inputData = require('prompt-sync')();
+
+type Data = {
+  cpf: string;
+  nome: string;
+  idade: number;
+  estadoCivil?: string;
+  endereco: string;
+  cidade: string;
+  estado: string;
+};
+
+const pessoas: {}[] = [];
+
+inputDados();
+function inputDados() {
+  const cpf = inputData('Informe seu CPF, somente numeros:');
+  if (cpf.length === 11) {
+    const dadosPessoais: Data = {
+      cpf: cpf,
+      nome: inputData('Informe seu Nome:'),
+      idade: inputData('Informe sua Idade:'),
+      estadoCivil: inputData('Informe o estado Civil:'),
+      endereco: inputData('Informe seu Endereco:'),
+      cidade: inputData('Informe sua Cidade:'),
+      estado: inputData('Informe seu Estado:'),
+    };
+    pessoas.push(dadosPessoais);
+  } else console.log('CPF inválido!');
+}
+
+if (pessoas.length > 0) {
+  for (let pessoa of pessoas) {
+    console.log(pessoa);
+  }
+} else {
+  console.log('Nenhum dado informado.');
+}
