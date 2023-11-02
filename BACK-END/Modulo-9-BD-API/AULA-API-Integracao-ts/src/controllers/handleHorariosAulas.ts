e669c1c97edae8e49541012ac531849e16463a74
Jorge H <jorgehenrrique@live.com>
2023-11-02 14:26:38 -0300

    refatorado horarioaulas, e adc update de horarioaulas

--- BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/controllers/handleHorariosAulas.ts
+++ BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/controllers/handleHorariosAulas.ts
@@ -7,8 +7,6 @@ export async function horariosAulasList(_: any, res: any) {
   const horarioAulaService = new HorarioAulaService(db);
 
   try {
-    // const result = await db.query('SELECT * FROM horario_aulas');
-    // res.json(result.rows);
     const horarioAulas = await horarioAulaService.getAll();
     res.json(horarioAulas);
   } catch (error: any) {
@@ -30,10 +28,8 @@ export async function horariosAulasListId(req: any, res: any) {
   const { id } = req.params;
 
   try {
-    const result = await db.query('SELECT * FROM horario_aulas WHERE id=$1', [
-      id,
-    ]);
-    res.json(result.rows[0]);
+    const horarioAula = await horarioAulaService.find(id);
+    res.json(horarioAula);
   } catch (error: any) {
     console.error(error);
     res.status(500).json({
@@ -50,21 +46,9 @@ export async function horariosAulasAdd(req: any, res: any) {
   await db.connect();
   const horarioAulaService = new HorarioAulaService(db);
 
-  const { dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id } =
-    req.body;
-
   try {
-    const query = `INSERT INTO horario_aulas (dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id)
-          VALUES ($1, $2, $3, $4, $5) Returning *;`;
-    const values = [
-      dia_semana,
-      hora_inicio,
-      hora_fim,
-      modalidade_id,
-      instrutor_id,
-    ];
-    const result = await db.query(query, values);
-    res.json(result.rows[0]);
+    const horarioAula = await horarioAulaService.create(req.body);
+    res.json(horarioAula);
   } catch (error: any) {
     res.status(500).json({
       error,
@@ -80,21 +64,12 @@ export async function horariosAulasUpdate(req: any, res: any) {
   await db.connect();
   const horarioAulaService = new HorarioAulaService(db);
 
-  const { dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id } =
-    req.body;
-
   try {
-    const query = `INSERT INTO horario_aulas (dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id)
-          VALUES ($1, $2, $3, $4, $5) Returning *;`;
-    const values = [
-      dia_semana,
-      hora_inicio,
-      hora_fim,
-      modalidade_id,
-      instrutor_id,
-    ];
-    const result = await db.query(query, values);
-    res.json(result.rows[0]);
+    const horarioAula = await horarioAulaService.update(
+      req.params.id,
+      req.body
+    );
+    res.json(horarioAula);
   } catch (error: any) {
     res.status(500).json({
       error,
--- BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/services/horariosAulas.service.ts
+++ BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/services/horariosAulas.service.ts
@@ -22,17 +22,48 @@ export class HorarioAulaService implements InterfaceCrud<HorarioAulasModel> {
   }
 
   async find(id: string): Promise<HorarioAulasModel> {
-    throw new Error('Method not implemented.');
+    const result = await this.db.query(
+      'SELECT * FROM horario_aulas WHERE id=$1',
+      [id]
+    );
+    return result.rows[0];
   }
 
   async create(payload: HorarioAulasModel): Promise<HorarioAulasModel> {
-    throw new Error('Method not implemented.');
+    const { dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id } =
+      payload;
+    const query = `INSERT INTO horario_aulas (dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id)
+      VALUES ($1, $2, $3, $4, $5) Returning *;`;
+    const values = [
+      dia_semana,
+      hora_inicio,
+      hora_fim,
+      modalidade_id,
+      instrutor_id,
+    ];
+    const result = await this.db.query(query, values);
+
+    return result.rows[0];
   }
 
   async update(
     id: string,
     payload: HorarioAulasModel
   ): Promise<HorarioAulasModel> {
-    throw new Error('Method not implemented.');
+    const { dia_semana, hora_inicio, hora_fim, modalidade_id, instrutor_id } =
+      payload;
+    const values = [
+      dia_semana,
+      hora_inicio,
+      hora_fim,
+      modalidade_id,
+      instrutor_id,
+      id,
+    ];
+    const query = `UPDATE horario_aulas
+    SET dia_semana=$1, hora_inicio=$2, hora_fim=$3, modalidade_id=$4, instrutor_id=$5
+    WHERE id=$6 Returning *;`;
+    const result = await this.db.query(query, values);
+    return result.rows[0];
   }
 }
