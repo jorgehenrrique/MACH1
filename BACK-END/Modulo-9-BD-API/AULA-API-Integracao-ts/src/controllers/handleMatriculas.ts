a420b87aaae8bd1e6efcd791c15accd7e984af24
Jorge H <jorgehenrrique@live.com>
2023-11-02 17:17:44 -0300

    otimizado updade em matriculas

--- BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/controllers/handleMatriculas.ts
+++ BACK-END/Modulo-9-BD-API/AULA-API-Integracao-ts/src/controllers/handleMatriculas.ts
@@ -63,9 +63,21 @@ export async function matriculasUpdate(req: any, res: any) {
   const db = createDBClient();
   await db.connect();
   const matriculaService = new MatriculaService(db);
+  const { id } = req.params;
 
   try {
-    const matricula = await matriculaService.update(req.params.id, req.body);
+    const matriculaId = await matriculaService.find(id);
+    const payload = {
+      aluno_id: req.body.aluno_id || matriculaId.aluno_id,
+      plano_id: req.body.plano_id || matriculaId.plano_id,
+      dia_vencimento: req.body.dia_vencimento || matriculaId.dia_vencimento,
+      valor_mensalidade:
+        req.body.valor_mensalidade || matriculaId.valor_mensalidade,
+      data_inicio: req.body.data_inicio || matriculaId.data_inicio,
+      data_fim: req.body.data_fim || matriculaId.data_fim,
+    };
+
+    const matricula = await matriculaService.update(id, payload);
     res.json(matricula);
   } catch (error: any) {
     res.status(500).json({
